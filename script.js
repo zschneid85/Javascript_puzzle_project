5eba5b7743333653234ab45a91d87306a4f96df2
Zach <zschneider.ninetyfive@gmail.com>
2021-11-16 13:23:20 -0500

    pushing all updated code for 11/16/2021

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let VIDEO=null;
 let CANVAS=null;
 let CONTEXT=null;
 let SCALER=0.6;
-let SIZE={x:0,y:0,width:0,height:0,row:0,columns:3};
+let SIZE={x:0,y:0,width:0,height:0,rows:0,columns:3};
 let PIECES=[];
 
 
@@ -18,20 +18,10 @@ function main (){
         VIDEO.play();
 
         VIDEO.onloadeddata=function() {
-            /*let resizer=SCALER*
-            Math.min(
-                window.innerWidth/VIDEO.videoWidth,
-                window.innerHeight/VIDEO.videoHeight
-            );
-            SIZE.width=resizer*VIDEO.videoWidth;
-            SIZE.height=resizer*VIDEO.videoHeight;
-            SIZE.x=window.innerWidth/2-SIZE.width/2;
-            SIZE.y=window.innerHeight/2-SIZE.height/2; */
-            
-            // Using an EventListenner to not use the above code
-
+           
             handleResizer();
             //window.addEventListener('resize',handleResizer);
+            initializePieces(SIZE.rows,SIZE.columns);
             updateCanvas();
         }
     }).catch(function(err){
@@ -56,17 +46,23 @@ function handleResizer() {
 }
 
 function updateCanvas() {
+    CONTEXT.clearRect(0,0,CANVAS.width,CANVAS.height);
+    CONTEXT.globalAlpha=0.5;
     CONTEXT.drawImage(VIDEO,
     SIZE.x, SIZE.y,
     SIZE.width, SIZE.height);
-    
+    CONTEXT.globalAlpha=1;
     for(let i=0; i<PIECES.length;i++) {
         PIECES[i].draw(CONTEXT);
     }
     window.requestAnimationFrame(updateCanvas);
 }
 
-function initializePieces(){
+function initializePieces(rows,cols){
+    SIZE.rows=rows;
+    SIZE.columns=cols;
+
+    
     PIECES=[];
     for(let i=0;i<SIZE.rows; i++){
         for(let j=0;j<SIZE.columns;j++){
@@ -74,7 +70,18 @@ function initializePieces(){
         }
     }
 }
-
+ function randomizePieces(){
+     for(let i=0; i<PIECES.length;i++){
+         let loc={
+             x:Math.random()*(CANVAS.width-PIECES[i].width),
+             y:Math.random()*(CANVAS.height-PIECES[i].height)
+        }
+        
+        PIECES[i].x=loc.x;
+        PIECES[i].y=loc.y;
+        
+    }
+ }
 class Piece{
     constructor(rowIndex,colIndex) {
         this.rowIndex=rowIndex;
@@ -82,11 +89,22 @@ class Piece{
         this.x=SIZE.x+SIZE.width*this.colIndex/SIZE.columns;
         this.y=SIZE.y+SIZE.height*this.rowIndex/SIZE.rows;
         this.width=SIZE.width/SIZE.columns;
-        this.height=SIZE.height/SIZE.rows;
+        this.height=SIZE.height/SIZE.row;
     
     }
     draw(context){
         context.beginPath();
+        
+        context.drawImage(VIDEO,
+            this.colIndex*VIDEO.videoWidth/SIZE.columns,
+            this.rowIndex*VIDEO.videoHeight/SIZE.rows,
+            VIDEO.videoWidth/SIZE.columns,
+            VIDEO.videoHeight/SIZE.rows,
+            this.x,
+            this.y,
+            this.width,
+            this.height);
+        
         context.rect(this.x,this.y,this.width,this.height);
         context.stroke();
     }
